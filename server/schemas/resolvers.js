ddaff9558398d535d8e69b1b108d76b8fe0eab5f
aaron <afarrell002@gmail.com>
2022-01-27 15:08:37 -0700

    created basic resolvers - queries for query users and single user, mutations for addUser and login

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,44 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError(`Must be a disconnected branch!
+          No user found with that email address!`);
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError(`Our roots go deep, but not that deep!
+          Incorrect email or password!`);
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
