81041d4904c0715439325870bb4190a5c11e2fce
aaron <afarrell002@gmail.com>
2022-01-28 15:36:00 -0700

    fixed typo in connectionjs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/demonDB", {
-  useNewUrlParses: true,
+  useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
