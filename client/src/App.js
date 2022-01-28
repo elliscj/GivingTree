3fa0f71d56126dd96d76eb9048472bc8679f45bd
Jason <LutzJason92@gmail.com>
2022-01-28 14:16:17 -0700

    Update App.js
    imported Roots-Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
-import Roots from "./components/Roots/Roots";
+import Roots from "./components/Roots-Dashboard/Roots";
 
 import NavBar from "./components/NavBar";
 // import Questions from "./components/questions";
@@ -10,7 +10,6 @@ import NavBar from "./components/NavBar";
 const App = () => {
   return (
     <HashRouter>
-      <Roots />
       <div>
         <Switch>
           <Route exact path="/" component={Landing} />
