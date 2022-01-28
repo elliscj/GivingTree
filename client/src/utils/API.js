15ab96b2be30f73416cd284fbf668bb9e4adf414
wwbarton <wbartonw5@gmail.com>
2022-01-28 12:37:17 -0700

    api key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,9 @@
 // Axios is a popular NPM package used for preforming API requests
-import axios from 'axios';
-
+import axios from "axios";
 
 const search = (query) =>
-  // axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}$&api_key=dc6zaTOxFJmzC&limit=20`);
+  axios.get(
+    `http://data.orghunter.com/v1/charitysearch?user_key=5e83606dc29f8eac5538826640e5758b${query}`
+  );
 
-export default search;
+export default { search };
