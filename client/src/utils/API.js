c616c3bac4855fb998eb2b8896b8ed894c5ff13a
Jason <LutzJason92@gmail.com>
2022-01-28 14:52:51 -0700

    Update API.js
    included a react fragment to eliminate red squiggles

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,9 @@
 // Axios is a popular NPM package used for preforming API requests
-import axios from 'axios';
+import axios from "axios";
 
-
-const search = (query) =>
+const search = (query) => (
   // axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}$&api_key=dc6zaTOxFJmzC&limit=20`);
-
+  // delet this fragment after axios.get is finalized
+  <></>
+);
 export default search;
