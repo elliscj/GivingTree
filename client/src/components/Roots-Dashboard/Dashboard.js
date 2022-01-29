b4b206d0f255b19d531f24d8e0f4704640c83a98
Jason <LutzJason92@gmail.com>
2022-01-28 21:40:39 -0700

    Update Dashboard.js
    styling and container for other components

--- client/src/components/Roots-Dashboard/Dashboard.js
+++ client/src/components/Roots-Dashboard/Dashboard.js
@@ -1,54 +1,37 @@
 import * as React from "react";
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import MuiDrawer from "@mui/material/Drawer";
 
+import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Badge from "@mui/material/Badge";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-
-import Link from "@mui/material/Link";
 import { Menu, ChevronLeft } from "@mui/icons-material";
 import {
-  Icon,
+  Badge,
+  Button,
   Box,
   Card,
   CardActions,
   CardContent,
   CardMedia,
-  Button,
-  Typography,
+  Container,
+  CssBaseline,
+  Divider,
+  Grid,
+  Icon,
+  IconButton,
+  List,
+  Link,
   Paper,
+  Typography,
 } from "@material-ui/core";
+
 import { mainListItems, secondaryListItems } from "./listItems";
+
 import Chart from "./Chart";
+import Donations from "./Donations";
 import Root from "../../assets/images/root.png";
 // import Deposits from "./Deposits";
 // import Orders from "./Orders";
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const drawerWidth = 200;
 
@@ -145,7 +128,7 @@ function DashboardContent() {
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "flex-end",
-                px: [5],
+                px: [1],
               }}
             >
               <IconButton onClick={toggleDrawer}>
@@ -170,14 +153,53 @@ function DashboardContent() {
             }}
           >
             <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            {/* <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
               <Grid container spacing={3}>
                 Chart
                 <Grid item xs={12} md={8} lg={9}>
                   <Chart />
                 </Grid>
               </Grid>
-              <Copyright sx={{ pt: 4 }} />
+            </Container> */}
+
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              <Grid container spacing={3}>
+                {/* Chart */}
+                <Grid item xs={12} md={8} lg={9}>
+                  <Paper
+                    sx={{
+                      p: 2,
+                      display: "flex",
+                      flexDirection: "column",
+                      height: 600,
+                    }}
+                  >
+                    <Chart />
+                  </Paper>
+                </Grid>
+                {/* Recent Deposits */}
+                <Grid item xs={12} md={4} lg={3}>
+                  <Paper
+                    sx={{
+                      p: 2,
+                      display: "flex",
+                      flexDirection: "column",
+                      height: 600,
+                    }}
+                  >
+                    <Donations />
+                  </Paper>
+                </Grid>
+                {/* Recent Orders */}
+                <Grid item xs={12}>
+                  <Paper
+                    sx={{ p: 2, display: "flex", flexDirection: "column" }}
+                  >
+                    <Chart />
+                  </Paper>
+                </Grid>
+              </Grid>
+              {/* <Copyright sx={{ pt: 4 }} /> */}
             </Container>
           </Box>
         </Box>
