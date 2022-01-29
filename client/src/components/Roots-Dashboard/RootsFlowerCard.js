48586d3b4b51c6fc7cf86d3141e1ec757ab63dad
Jason <LutzJason92@gmail.com>
2022-01-28 19:39:32 -0700

    Update RootsFlowerCard.js
    card wireframed

--- client/src/components/Roots-Dashboard/RootsFlowerCard.js
+++ client/src/components/Roots-Dashboard/RootsFlowerCard.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {
+  Paper,
   Box,
   Card,
   CardActions,
@@ -8,29 +9,35 @@ import {
   Button,
   Typography,
 } from "@material-ui/core";
-import Flower from "../../assets/images/flower.png";
-import "./Roots.css";
+
+import "../../styles/Roots.css";
 
 export default function FlowerCard() {
   return (
-    <Card sx={{ maxWidth: 345 }} className="rootcard" container spacing={6}>
-      <CardMedia
-        className="icons"
-        component="img"
-        image={Flower}
-        alt="flower icon"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Your Flowers
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Flowers represent your individual Donations
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="medium">View All Flowers</Button>
-      </CardActions>
-    </Card>
+    <>
+      <Paper elevation={8} className="paper">
+        <Card xs={{ maxWidth: 345 }} className="rootcard">
+          {/* <CardMedia
+            className="icons"
+            component="img"
+            image={Flower}
+            alt="flower icon"
+          /> */}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Your Flowers
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Flowers represent your individual Donations
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button className="cardButtons" size="large">
+              View All Flowers
+            </Button>
+          </CardActions>
+        </Card>
+      </Paper>
+    </>
   );
 }
