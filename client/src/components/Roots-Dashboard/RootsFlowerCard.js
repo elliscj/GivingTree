7e740231481f5fef2e69ed612e48b3cb53a7244f
Jason <LutzJason92@gmail.com>
2022-01-29 19:54:02 -0700

    Cards
    Removed Paper on Paper,
    Wrapped Crad Header in Title

--- client/src/components/Roots-Dashboard/RootsFlowerCard.js
+++ client/src/components/Roots-Dashboard/RootsFlowerCard.js
@@ -9,35 +9,33 @@ import {
   Button,
   Typography,
 } from "@material-ui/core";
-
+import Title from "./Title";
 import "../../styles/Roots.css";
 
 export default function FlowerCard() {
   return (
     <>
-      <Paper elevation={8} className="paper">
-        <Card xs={{ maxWidth: 345 }} className="rootcard">
-          {/* <CardMedia
+      <Card xs={{ maxWidth: 345 }} className="rootcard">
+        {/* <CardMedia
             className="icons"
             component="img"
             image={Flower}
             alt="flower icon"
           /> */}
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Your Flowers
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Flowers represent your individual Donations
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button className="cardButtons" size="large">
-              View All Flowers
-            </Button>
-          </CardActions>
-        </Card>
-      </Paper>
+        <CardContent>
+          <Title gutterBottom variant="h5" component="div">
+            Your Flowers
+          </Title>
+          <Typography variant="body1" color="text.secondary">
+            Flowers represent your individual Donations
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button className="cardButtons" size="large">
+            View All Flowers
+          </Button>
+        </CardActions>
+      </Card>
     </>
   );
 }
--- client/src/components/Roots-Dashboard/RootsLeafCard.js
+++ client/src/components/Roots-Dashboard/RootsLeafCard.js
@@ -11,33 +11,31 @@ import {
 } from "@material-ui/core";
 import Leaf from "../../assets/images/leaf.png";
 import "../../styles/Roots.css";
-
+import Title from "./Title";
 export default function LeafCard() {
   return (
     <>
-      <Paper elevation={8} className="paper">
-        <Card xs={{ maxWidth: 345 }} className="rootcard">
-          {/* <CardMedia
+      <Card xs={{ maxWidth: 345 }} className="rootcard">
+        {/* <CardMedia
             className="icons"
             component="img"
             image={Leaf}
             alt="flower icon"
           /> */}
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Your Leafs
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Leafs represent your Favorite Organizations
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button className="cardButtons" size="large">
-              View All Leafs
-            </Button>
-          </CardActions>
-        </Card>
-      </Paper>
+        <CardContent>
+          <Title gutterBottom variant="h5" component="div">
+            Your Leafs
+          </Title>
+          <Typography variant="body1" color="text.secondary">
+            Leafs represent your Favorite Organizations
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button className="cardButtons" size="large">
+            View All Leafs
+          </Button>
+        </CardActions>
+      </Card>
     </>
   );
 }
