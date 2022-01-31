66d1b91a28656f3b6b9ceaf86e9b8b4df5ddaa86
Jason <LutzJason92@gmail.com>
2022-01-31 13:39:18 -0700

    Update App.js
    typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import Landing from "./components/landing";
+import Landing from "./components/Landing";
 import Questions from "./components/questions";
 import CharityIndex from "./components/charityIndex";
 // import Roots from "./components/Roots-Dashboard/Roots";
