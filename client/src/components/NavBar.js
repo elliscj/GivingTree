d262af0ea2b4358a834debbdde09f741d6b99910
Jason <LutzJason92@gmail.com>
2022-01-31 12:04:13 -0700

    Update NavBar.js
    added other pages to match navbar.
    
    forgot to do a gitpull and didnt realize a navbar was written.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,7 +12,13 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 
-const pages = ["About Us", "Search Charities"];
+const pages = [
+  "Home",
+  "About Us",
+  "Search Charities",
+  "Your Leafs",
+  "Contact Us",
+];
 const settings = ["Sign In", "Sign Up", "Logout", "Dashboard"];
 
 const ResponsiveAppBar = () => {
