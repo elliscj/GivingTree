882356ad5820dc0a5af9b2d9bef0382b08f529c3
MohEssmat <92461972+MohEssmat@users.noreply.github.com>
2022-01-30 20:42:51 -0700

    NO change

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./App.css";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
-import Roots from "./components/Roots-Dashboard/Roots";
+// import Roots from "./components/Roots-Dashboard/Roots";
 
 import NavBar from "./components/NavBar";
 // import Questions from "./components/questions";
 
 const App = () => {
   return (
-    <Roots />
-    // <HashRouter>
-    //   <div>
-    //     <Switch>
-    //       <Route exact path="/" component={Landing} />
-    //       <Route exact path="/components/NavBar" component={NavBar} />
-    //     </Switch>
-    //   </div>
-    // </HashRouter>
+    // <Roots />
+    <HashRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/components/NavBar" component={NavBar} />
+        </Switch>
+      </div>
+    </HashRouter>
   );
 };
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,37 @@
 import React from "react";
 
 function NavBar() {
-  return <div></div>;
+  const linkStyle = { border: "1px black", padding: "5px" };
+
+  return (
+    <nav className="main-header-menu">
+      <section
+        style={{
+          display: "flex",
+          fontFamily: "helvetica",
+          flexDirection: "row",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+        }}
+      >
+        <div style={linkStyle}>
+          <a href="#">Home</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Your leaves</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Contact Us</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Login</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Sign-up</a>
+        </div>
+      </section>
+    </nav>
+  );
 }
 
 export default NavBar;
