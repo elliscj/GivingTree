27e5b5a46c335e023c59d4140960c5d90e701914
Jason <LutzJason92@gmail.com>
2022-01-31 12:26:11 -0700

    Update SignUp.js
    typo

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -79,7 +79,7 @@ export default function SignUp() {
                   fullWidth
                   id="username"
                   label="Username"
-                  name="usrname"
+                  name="username"
                   autoComplete="username"
                   autoFocus
                 />
