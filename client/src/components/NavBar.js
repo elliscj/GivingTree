039e758702cb25c44f1fd99235f7d1e9a3496192
Jason <LutzJason92@gmail.com>
2022-01-31 16:31:25 -0700

    Update NavBar.js
    imported icon for menu drop down

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,6 +11,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
+import Root from "../assets/images/root.png";
 
 const pages = [
   "Home",
