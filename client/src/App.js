72ee0eb1212253a60cc21db1a07e02c534e11ee1
MohEssmat <92461972+MohEssmat@users.noreply.github.com>
2022-01-31 15:49:17 -0700

    fixed landing page & added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,23 +5,20 @@ import Landing from "./components/Landing";
 import Questions from "./components/questions";
 import CharityIndex from "./components/charityIndex";
 // import Roots from "./components/Roots-Dashboard/Roots";
-// import NavBar from "./components/NavBar";
+import ResponsiveAppBar from "./components/NavBar";
 
 const App = () => {
   return (
     // <Roots />
     <BrowserRouter>
       <div>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          {/* <Route exact path="/components/NavBar" component={NavBar} /> */}
-          <Route exact path="/components/questions" component={Questions} />
-          <Route
-            exact
-            path="/components/charityIndex"
-            component={CharityIndex}
-          />
-        </Switch>
+        {/* <Switch> */}
+        <Route path="/components/" component={ResponsiveAppBar} />
+        <Route exact path="/" component={Landing} />
+        {/* <Route exact path="/components/NavBar" component={NavBar} /> */}
+        <Route exact path="/components/questions" component={Questions} />
+        <Route exact path="/components/charityIndex" component={CharityIndex} />
+        {/* </Switch> */}
       </div>
     </BrowserRouter>
   );
