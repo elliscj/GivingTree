1618eef0751a0c15321167be605cba49ca7f9a67
MohEssmat <92461972+MohEssmat@users.noreply.github.com>
2022-02-01 21:11:39 -0700

    added API key to .env file

--- server/routes/api/apiRoutes.js
+++ server/routes/api/apiRoutes.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 const router = require("express").Router;
+const appId = process.env.REACT_APP_APP_ID;
+const appKey = process.env.REACT_APP_APP_KEY;
 
 router.get("/api/questions/:city/:state/:category", (req, res) => {
   const city = req.params.city;
@@ -7,7 +9,7 @@ router.get("/api/questions/:city/:state/:category", (req, res) => {
   const category = req.params.category;
   axios
     .get(
-      `https://api.data.charitynavigator.org/v2/Organizations?app_id=1f111d45&app_key=45644615277bb0eaacf2e9e76d6fb930&state=${state}&city=${city}`
+      `https://api.data.charitynavigator.org/v2/Organizations?app_id=${appId}&app_key=${appKey}&state=${state}&city=${city}`
     )
     .then((data) => {
       res.json(data);
